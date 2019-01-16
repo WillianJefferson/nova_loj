96b3e00be20576467bd31b3656508fdd216a1371
Willian Jefferson <willianjefferson007@hotmail.com>
2019-01-16 18:36:53 -0200

    Arquivo de teste deletado
    Arquivo de teste deletado

--- teste2.php
+++
@@ -1,6 +0,0 @@
-<!-- INICIO BOTAO BOLETO FACIL - NAO EDITAR -->
-<form action="https://www.boletobancario.com/boletofacil/integration/button/checkout.html" method="post">
-<input type="hidden" name="code" value="883999F3227601DD" />
-<input type="image" src="https://www.boletobancario.com/boletofacil/img/payment-buttons/button-box-medium.png" name="submit" alt="Pague com Boleto Banc&aacute;rio!" />
-</form>
-<!-- FINAL BOTAO BOLETO FACIL -->
